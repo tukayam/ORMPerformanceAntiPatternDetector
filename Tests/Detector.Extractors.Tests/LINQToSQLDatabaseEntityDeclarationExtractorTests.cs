d47bc0907e2dd097271899e47a49d2513c9209b8
Tuba Kaya <tuba_kaya@outlook.com>
2015-05-19 16:59:18 +0200

    small adjustments

---
+++ Tests/Detector.Extractors.Tests/LINQToSQLDatabaseEntityDeclarationExtractorTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Detector.Extractors.DatabaseEntities;
+
+namespace Detector.Extractors.Tests
+{
+    [TestClass]
+    public class LINQToSQLDatabaseEntityDeclarationExtractorTests
+    {
+        LINQToSQLDatabaseEntityDeclarationExtractor target;
+
+        [TestMethod]
+        public void DetectsDatabaseEntityDeclarations()
+        {
+        }
+    }
+}
